66f3bbb210be865664fcec7c51a05eb22b7d66bc
wangwei <wangwei01860@hellobike.com>
2018-09-21 17:24:50 +0800

    增加复制功能组件，可直接通过引入组件的方式使用

---
+++ index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-button
+      type="primary"
+      @click="doCopy(value)">
+      复制
+    </el-button>
+  </div>
+</template>
+
+<script>
+import { Button } from 'element-ui';
+import { listener, handleCopy } from './copyCommand.js';
+
+export default {
+  name: 'CopyToClipboard',
+  components: {
+    elButton: Button,
+  },
+  mixins: [listener],
+  props: {
+    value: {
+      type: String,
+      default: null,
+    }
+  },
+  methods: {
+    doCopy(data) {
+      handleCopy(data);
+    }
+  },
+};
+</script>
+
+<style>
+
+</style>
